f3a54747862ccec018a42337431eeb54831be672
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-27 00:45:42 +0200

    Add files via upload

---
+++ src/Pages/Details/Details.js
@@ -0,0 +1,59 @@
+import React from "react";
+import "./Details.scss";
+import { Context } from "../../context";
+import Back from "../../assets/icons/left-arrow.svg";
+
+class Details extends React.Component {
+  state = {
+    styles: {
+      backgroundColor: this.context.itemForDetailPage.bg_color,
+      transform: `translate(${this.context.cords.x}px, ${this.context.cords.y}px)`,
+      maxWidth: `${this.context.cords.width}px`,
+      height: `${this.context.cords.height}px`,
+      borderRadius: "30px"
+    }
+  };
+  componentDidMount() {
+    window.scrollTo(0, 0);
+    this.context.getDetailPageItem(this.props.match.params.itemId);
+    setTimeout(() => {
+      const styles = {
+        backgroundColor: this.context.itemForDetailPage.bg_color
+      };
+      this.setState({ styles });
+    });
+  }
+
+  render() {
+    const { itemForDetailPage } = this.context;
+    const {
+      title,
+      img_url,
+      description
+    } = itemForDetailPage;
+    return (
+      <div className="details-wrapper">
+        <div
+          className="img-wrapper"
+          style={this.state.styles}
+          id={"img-wrapper"}
+        >
+          <img src={img_url} alt={title} />
+        </div>
+        <div className="details">
+          <span onClick={this.props.history.goBack} className="back">
+            <img src={Back} alt="back" width={20} />
+            Back to Home
+          </span>
+          <div className="detail-box">
+            <h2>{title}</h2>
+          </div>
+          <div className="detail-box">
+          </div>
+        </div>
+      </div>
+    );
+  }
+}
+Details.contextType = Context;
+export default Details;
---
+++ src/data.js
@@ -0,0 +1,55 @@
+export const intro = {
+  title: "Welcome to Covid19-Information center!!!",
+  description: "Select any service to view its details."
+};
+
+export const list = [
+  {
+    id: "news",
+    title: "News",
+    img_url: "../images/news.jpg",
+    bg_color: "#ffa21c",
+  },
+  {
+    id: "symptoms",
+    title: "Symptoms",
+    img_url: "../images/symptoms.png",
+    bg_color: "#d2e8f2",
+  },
+  {
+    id: "prevention",
+    title: "Prevention",
+    img_url: "../images/prevention.png",
+    bg_color: "#d6cc9e",
+  },
+  {
+    id: "movies",
+    title: "Movies",
+    img_url: "../images/movies.jpeg",
+    bg_color: "#d64e00",
+  },
+  {
+    id: "games",
+    title: "Games",
+    img_url: "../images/games.jpg",
+    bg_color: "#ffe0b5",
+  },
+  {
+    id: "statics",
+    title: "Statistics",
+    img_url: "../images/static.png",
+    bg_color: "#9bc6ba",
+  },
+  {
+    id: "disc",
+    title: "Discussions",
+    img_url: "../images/disc.png",
+    bg_color: "#e6c5d0",
+  },
+  {
+    id: "aboutus",
+    title: "AboutUs",
+    img_url: "../images/aboutus.jpeg",
+    bg_color: "#ffe083",
+  }
+];
