1d651bdb4d9eac8ff72e8a51bb4567ed213080a4
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-27 01:36:37 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -33,6 +33,32 @@ export const list = [
     title: "Games",
     img_url: "../images/joystick.png",
     bg_color: "#ffe0b5",
+    description: 
+    "https://houseparty.com/
+
+Houseparty is a video chat app owned by Fortnite developer Epic Games that lets you play popular games like trivia and Heads Up with friends online. While the app is free to download, you do have to pay for some of the games, typically $1-$3. Up to eight people can join a video chat "room" at once and play games together. 
+
+
+https://animal-crossing.com/
+
+Animal Crossing: New Horizons came out on Nintendo Switch in March, right at the start of many coronavirus shelter-in-place orders. It's taken the world by storm, and its social aspect has made social distancing a bit easier. While you can perform simple tasks like growing fruit, planting trees and designing rooms alone, you can also invite friends to visit your island and go fishing and exploring together. If you don't have a Switch to play on, you can also check out the mobile version, Animal Crossing: Pocket Camp. 
+
+https://skribbl.io/
+
+skribbl.io is a free multiplayer drawing and guessing game.
+One game consists of a few rounds in which every round someone has to draw their chosen word and others have to guess it to gain points!
+The person with the most points at the end of game will then be crowned as the winner!
+
+https://ludoking.com/
+
+Ludo is a strategy board game for two to four players, in which the players race their four tokens from start to finish according to the rolls of a single die. Like other cross and circle games, Ludo is derived from the Indian game Pachisi, but simpler.
+
+https://www.letsplayuno.com/
+
+UNO™, the world's most beloved card game with new experience. Begin a new adventure with the friends across the world now!
+
+
+    "
   },
   {
     id: "statistics",
