8a46ca3eee5e7cda4c42e732eaacbe8ecff348c2
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-28 00:00:58 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -5,44 +5,44 @@ export const intro = {
 
 export const list = [
   {
-    id: "news",
-    title: "News",
+    id: "lajmet",
+    title: "Lajmet",
     img_url: "../images/marketing.png",
     bg_color: "#ffa21c",
   },
   {
-    id: "symptoms",
-    title: "Symptoms",
+    id: "simptomat",
+    title: "Simptomat",
     img_url: "../images/covid(1).png",
     bg_color: "#d2e8f2",
     description: "Simptomat fillestare që shfaqen tek personat e infektuar me virusin COVID – 19 janë: Ethe dhe temperaturë e lartë; Kollitje e rëndë; Vështirësi në frymëmarrje. Sëmundja mund të shkaktojë komplikime serioze të cilat mund të dëmtojnë funksionin e: Organeve të frymëmarrjes; Sistemit kardiovaskular; Të veshkave. Sëmundja mund të ndryshojë nga forma e lehtë deri në formë më të rëndë, me pasoja të mundshme fatale. Mjekimi i të infektuarve është paliativ dhe simptomatik. Në këtë moment ende nuk ka terapi kundër këtij virusi, e as më pak vaksinë për parandalim nga infektimi me COVID – 19.",
     embed: "https://vlada.mk/node/20495?ln=sq"
   },
   {
-    id: "prevention",
-    title: "Prevention",
+    id: "parandalimi",
+    title: "Parandalimi",
     img_url: "../images/covid(2).png",
     bg_color: "#d6cc9e",
     description: "Koronavirusi shkakton simptoma tipike respiratore. Më poshtë paraqiten rekomandimet kryesore: Lani shpesh duart me ujë dhe sapun ose pastrojini ato duke i fërkuar me pastrues me bazë alkooli; Kur kolliteni apo teshtini, mbuloni gojën dhe hundën me gropëzën e bërrylit ose shami letre. Hidheni menjëherë shaminë e letrës në koshin e plehrave dhe pastaj lani duart; Mënjanoni kontaktet e ngushta me këdo që ka temperaturë dhe kollë; Mbani një distancë të caktuar – të paktën një metër – nga njerëzit e tjerë, veçanërisht kur kolliten ose teshtijnë ose nëse kanë ethe, sepse virusi gjendet në spërklat e pështymës dhe mund të transmetohet nëpërmjet frymëmarrjes në distancë të afërt. Paraqituni tek mjeku nëse keni kollë, temperaturë apo vështirësi në frymëmarrje dhe raportoni historinë e udhëtimeve nëse keni udhëtuar kohët e fundit; Mënjanoni kontaktet me tregjet e kafshëve të gjalla në zonat që kanë Koronavirus dhe parandaloni kontaktet e pambrojtura me kafshë të gjalla apo sipërfaqe që kanë qenë në kontakt me kafshët;",
     embed: "https://covid.tirana.al/si-te-parandalojme-dhe-minimizojme-perhapjen-e-covid-19/"
   },
   {
-    id: "statistics",
-    title: "Statistics",
+    id: "statistikat",
+    title: "Statistikat",
     img_url: "../images/covid.png",
     bg_color: "#9bc6ba",
     embed: "https://amorphouswelshcorgi.htmlpasta.com/"
     
   },
   {
-    id: "disc",
-    title: "Discussions",
+    id: "chat",
+    title: "Chat",
     img_url: "../images/communication.png",
     bg_color: "#e6c5d0",
   },
   {
-    id: "aboutus",
-    title: "About Us",
+    id: "rreth",
+    title: "Rreth Nesh",
     img_url: "../images/responsive.png",
     bg_color: "#ffe083",
     description: "Pr0ject-19 is a website built to help users and people around to get quick and easy information about coronavirus (COVID-19) such as : first syptoms check , how to take care of yourself , get movie and games suggestions , real time statistics etc. \n\n This Project was developed and managed from : \n\n Nevis Hysenaj (Web Developer / InfoSec Researcher) \n\n Erda Ymeri (Application Developer / Pentester) \n\n Suljot Gjoka (Pentester , Exploit Development , Web Design)", 
