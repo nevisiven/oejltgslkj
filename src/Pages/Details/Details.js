7e0395c9a07f9bfe17d33122598549b7b65785f7
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-27 14:56:40 +0200

    Update Details.js

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -56,7 +56,7 @@ class Details extends React.Component {
           <div className="detail-box">
             <p>{link}</p>
           <div className="detail-box">
-           <embed type="text/html" src="{embed}"></embed>
+           <embed type="text/html" src={embed} ></embed>
           </div>
           </div>
         </div>
