f97490819e31271f37832503c3fcc3c0fdb8b4b5
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-27 02:21:58 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -33,6 +33,10 @@ export const list = [
     title: "Games",
     img_url: "../images/joystick.png",
     bg_color: "#ffe0b5",
+    title: "1.House Party",
+    link: "https://houseparty.com/",
+    description: "Houseparty is a video chat app owned by Fortnite developer Epic Games that lets you play popular games like trivia and Heads Up with friends online.",
+    title: "2.Animal Crossing",
     description: "Houseparty is a video chat app owned by Fortnite developer Epic Games that lets you play popular games like trivia and Heads Up with friends online.",
     link: "https://houseparty.com/"
   },
