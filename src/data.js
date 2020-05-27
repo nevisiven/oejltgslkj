a747d8b750a6e0eda5280efbfb099173b4c28d9a
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-28 00:58:54 +0200

    Update data.js

--- src/data.js
+++ src/data.js
@@ -1,18 +1,18 @@
 export const intro = {
-  title: "Welcome to Covid19-Information Center!!!",
-  description: "Zgjidhni nje nga "
+  title: "Impact of Corona Virus",
+  description: " "
 };
 
 export const list = [
   {
-    id: "lajmet",
-    title: "Lajmet",
+    id: "news",
+    title: "News",
     img_url: "../images/marketing.png",
     bg_color: "#ffa21c",
   },
   {
-    id: "statistikat",
-    title: "Statistikat",
+    id: "stats",
+    title: "Statistics",
     img_url: "../images/covid.png",
     bg_color: "#9bc6ba",
     embed: "https://amorphouswelshcorgi.htmlpasta.com/"
@@ -25,8 +25,8 @@ export const list = [
     embed: "https://hack.chat/?8qabqcua"
   },
   {
-    id: "rreth",
-    title: "Rreth Nesh",
+    id: "about",
+    title: "About Us",
     img_url: "../images/responsive.png",
     bg_color: "#ffe083",
     description: "Pr0ject-19 is a website built to help users and people around to get quick and easy information about coronavirus (COVID-19) such as : first syptoms check , how to take care of yourself , get movie and games suggestions , real time statistics etc. \n\n This Project was developed and managed from : \n\n Nevis Hysenaj (Web Developer / InfoSec Researcher) \n\n Erda Ymeri (Application Developer / Pentester) \n\n Suljot Gjoka (Pentester , Exploit Development , Web Design)", 
