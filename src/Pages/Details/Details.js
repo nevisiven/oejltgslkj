b60f5a7b5c2ba4c087d0765d309443f61176e5b5
nevisiven <47783863+nevisiven@users.noreply.github.com>
2020-05-27 02:34:11 +0200

    Update Details.js

--- src/Pages/Details/Details.js
+++ src/Pages/Details/Details.js
@@ -53,6 +53,13 @@ class Details extends React.Component {
           <div className="detail-box">
            <p>{description}</p>
           </div>
+      <div className="detail-box">
+           <href>{link}</href>
+          </div>
+      <div className="detail-box">
+           <img src={img_det}/>
+          </div>
+      
         </div>
       </div>
     );
